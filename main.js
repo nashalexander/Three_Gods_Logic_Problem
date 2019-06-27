05d94d0877159d67a755fc3eccbbb71e52e02a7a
Alex Nash <nashar@umich.edu>
2019-06-27 14:47:25 -0400

    found a solution

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ class God{
         this.name = name;
     }
 
-    ask(bool){
-        Num_questions++;
+    ask(bool, sub_q = false){
+        if(!sub_q) Num_questions++;
+
         if(this.name == god_names.true){
             if(bool) return yes_word;
             else return no_word;
@@ -82,17 +83,67 @@ function setup(){
     temp.splice(0,1);
 }
 
-function test(A_name,B_name,C_name){
+function test(A_name,B_name,C_name, quite = false){
+    let result;
     if((A.name == A_name) && (B.name == B_name) && (B.name == B_name)){
-        console.log("Correct");
+        if(!quite) console.log("Correct");
+        result = true;
     }
     else{
-        console.log("Incorrect");
+        if(!quite) console.log("Incorrect");
+        result = false;
     }
-    console.log("A : " + A.name);
-    console.log("B : " + B.name);
-    console.log("C : " + C.name);
-    console.log("Number of questions: "+ Num_questions);
+    if(!quite){
+        console.log("A : " + A.name);
+        console.log("B : " + B.name);
+        console.log("C : " + C.name);
+        console.log("Number of questions: "+ Num_questions);
+    }
+
+    return result;
 }
 
 window.onload = setup();
+
+solution = function(quite = false){
+    let answer = ["","",""];
+
+    let q_1 = A.ask(A.ask(B.name == god_names.random,true) == "da");
+    if(q_1 == "da"){
+        let q_2 = C.ask(C.ask(C.name == god_names.true,true) == "da");
+        if(q_2 == "da") answer[2] = god_names.true;
+        else if (q_2 == "ja") answer[2] = god_names.false;
+        
+        let q_3 = C.ask(C.ask(A.name == god_names.random,true) == "da");
+        if(q_3 == "da"){
+            answer[0] = god_names.random;
+            if(answer[2] == god_names.true) answer[1] = god_names.false;
+            else if(answer[2] == god_names.false) answer[1] = god_names.true;
+        }
+        else if(q_3 == "ja"){
+            answer[1] = god_names.random;
+            if(answer[2] == god_names.true) answer[0] = god_names.false;
+            else if(answer[2] == god_names.false) answer[0] = god_names.true;
+        }
+    }
+    else if(q_1 == "ja"){
+        let q_2 = B.ask(B.ask(B.name == god_names.true,true) == "da");
+        if(q_2 == "da") answer[1] = god_names.true;
+        else if (q_2 == "ja") answer[1] = god_names.false;
+        
+        let q_3 = B.ask(B.ask(A.name == god_names.random,true) == "da");
+        if(q_3 == "da"){
+            answer[0] = god_names.random;
+            if(answer[1] == god_names.true) answer[2] = god_names.false;
+            else if(answer[1] == god_names.false) answer[2] = god_names.true;
+        }
+        else if(q_3 == "ja"){
+            answer[2] = god_names.random;
+            if(answer[1] == god_names.true) answer[0] = god_names.false;
+            else if(answer[1] == god_names.false) answer[0] = god_names.true;
+        }
+    }
+
+    return test(answer[0],answer[1],answer[2],quite);
+
+}
